b40e8ad7a7294d0182800bf47df73213d3681dd5
Renato P Eduardo Jr <renatojr@outlook.com>
2023-03-29 12:46:40 -0300

    Adding new methods to save images

--- src/main/java/io/rjdev/booster/util/img/ImgUtil.java
+++ src/main/java/io/rjdev/booster/util/img/ImgUtil.java
@@ -1,24 +1,34 @@
 package io.rjdev.booster.util.img;
 
 import java.awt.AlphaComposite;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
+import java.util.Locale;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageOutputStream;
 
 public class ImgUtil {
 
-	private static int DEFAULT_THUMB_WIDTH = 244;
-	private static int DEFAULT_THUMB_HEIGHT = 244;
+	private static final int DEFAULT_THUMB_WIDTH = 244;
+	private static final int DEFAULT_THUMB_HEIGHT = 244;
+	private static final String FORMAT_JPG = "JPEG";
+	private static final String FORMAT_PNG = "PNG";
+	public static final String EXT_JPG = "jpg";
+	public static final String EXT_PNG = "png";
 
 	public static boolean writeImage(BufferedImage bufImage, String extensao, File imageFile){
 		boolean success = true;
@@ -51,7 +61,7 @@ public class ImgUtil {
 	public static BufferedImage generateThumbnail(BufferedImage original,
 			Integer width, Integer height, String ext) {
 
-		boolean isPNG = ext.toLowerCase().equals("png");
+		boolean isPNG = ext.toLowerCase().equals(FORMAT_PNG);
 
 		Image scaled = (width != null && height != null) ? original
 				.getScaledInstance(width, height, Image.SCALE_SMOOTH)
@@ -94,8 +104,8 @@ public class ImgUtil {
 	public static boolean writeJpgMaxHigh(BufferedImage bImage, String ext,
 			File file) {
 
-		if (!(ext.toUpperCase().equals("JPG")
-				|| ext.toUpperCase().equals("JPEG")))// Somente JPG
+		if (!(ext.toLowerCase().equals(ImgUtil.EXT_JPG)
+				|| ext.toLowerCase().equals(FORMAT_JPG)))// Somente JPG
 			return false;
 
 		try{
@@ -120,5 +130,86 @@ public class ImgUtil {
 		}
 		return true;
 	}
+
+
+	public static String getFormat(final InputStream stream) throws IOException {
+        final ImageInputStream iis = ImageIO.createImageInputStream(stream);
+        final Iterator<ImageReader> iter = ImageIO.getImageReaders(iis);
+        if (!iter.hasNext()) {
+            throw new IOException("Unsupported image format!");
+        }
+        final ImageReader reader = iter.next();
+        iis.close();
+        return reader.getFormatName();
+    }
+
+    public static BufferedImage loadImage(final InputStream stream) throws IOException {
+        return ImageIO.read(stream);
+    }
+
+    public static boolean saveImageAsJPEG(final BufferedImage image, final OutputStream stream) {
+        try {
+			return ImageIO.write(image, FORMAT_JPG, stream);
+		} catch (IOException e) {
+			System.out.println("ERROR ImgUtil.saveImageAsJPEG: "+ e.getMessage());
+			return false;
+		}finally{
+			try {
+				stream.close();
+			} catch (IOException e) {}
+		}
+    }
+
+    public static boolean saveImageAsJPEG(final BufferedImage image, final OutputStream stream, final int qualityPercent) {
+		if (qualityPercent < 0 || qualityPercent > 100) {
+			throw new IllegalArgumentException("Quality out of bounds!");
+		}
+		try {
+			final float quality = qualityPercent / 100.0f;
+			ImageWriter writer = null;
+			final Iterator<ImageWriter> iter = ImageIO.getImageWritersByFormatName(FORMAT_JPG);
+			if (iter.hasNext()) {
+				writer = iter.next();
+			}
+			final ImageOutputStream ios = ImageIO.createImageOutputStream(stream);
+			writer.setOutput(ios);
+			final ImageWriteParam iwparam = new JPEGImageWriteParam(Locale.getDefault());
+			iwparam.setCompressionMode(2);
+			iwparam.setCompressionQuality(quality);
+			writer.write(null, new IIOImage(image, null, null), iwparam);
+			ios.flush();
+			writer.dispose();
+			ios.close();
+			return true;
+		} catch (Exception e) {
+			System.out.println("ERROR ImgUtil.saveImageAsJPEG: "+ e.getMessage());
+			return false;
+		}
+	}
+
+	public static boolean saveImageAsPNG(final BufferedImage image, final OutputStream stream){
+		try {
+			return ImageIO.write(image, FORMAT_PNG, stream);
+		} catch (IOException e) {
+			System.out.println("ERROR ImgUtil.saveImageAsPNG: "+ e.getMessage());
+			return false;
+		}finally{
+			try {
+				stream.close();
+			} catch (IOException e) {}
+		}
+	}
+
+    private static BufferedImage convertToARGB(final BufferedImage srcImage) {
+        final BufferedImage newImage = new BufferedImage(
+			srcImage.getWidth(null),
+			srcImage.getHeight(null),
+			BufferedImage.TYPE_INT_ARGB
+		);
+        final Graphics bg = newImage.getGraphics();
+        bg.drawImage(srcImage, 0, 0, null);
+        bg.dispose();
+        return newImage;
+    }
 
 }
--- src/test/java/io/rjdev/booster/util/img/ImgUtilTest.java
+++ src/test/java/io/rjdev/booster/util/img/ImgUtilTest.java
@@ -2,7 +2,10 @@ package io.rjdev.booster.util.img;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -27,14 +30,68 @@ public class ImgUtilTest {
 
     @Test
     public void test_writeImage_generateThumbnail(){
-        assert(ImgUtil.writeImage(bufImage, "jpg", new File("src/main/resources/200.jpg")));
+        assert ImgUtil.writeImage(bufImage, ImgUtil.EXT_JPG, new File("src/main/resources/200.jpg"));
         BufferedImage bufThumb = ImgUtil.generateThumbnail(bufImage, null, null, "jpg");
-        assert(ImgUtil.writeImage(bufThumb, "jpg", new File("src/main/resources/200t.jpg")));
+        assert ImgUtil.writeImage(bufThumb, ImgUtil.EXT_JPG, new File("src/main/resources/200t.jpg"));
     }
 
     @Test
     public void test_writeJpgMaxHigh(){
-        assert(ImgUtil.writeJpgMaxHigh(bufImage, "jpg", new File("src/main/resources/200.jpg")));
+        assert ImgUtil.writeJpgMaxHigh(bufImage, ImgUtil.EXT_JPG, new File("src/main/resources/200.jpg"));
+    }
+
+    @Test
+    public void test_getFormat(){
+        try {
+            String format = ImgUtil.getFormat(new FileInputStream("src/main/resources/200t.jpg"));
+            System.out.println(format);
+            assert "JPEG".equals(format);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_loadImage(){
+        try {
+            BufferedImage bi = ImgUtil.loadImage(new FileInputStream("src/main/resources/200t.jpg"));
+            assert(bi != null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_saveImageAsJPEG(){
+        try {
+            OutputStream outs = new FileOutputStream(new File("src/main/resources/200.jpg"));
+            assert ImgUtil.saveImageAsJPEG(bufImage, outs);
+            outs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_saveImageAsJPEGQuality(){
+        try {
+            OutputStream outs = new FileOutputStream(new File("src/main/resources/200.jpg"));
+            assert ImgUtil.saveImageAsJPEG(bufImage, outs, 60);
+            outs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_saveImageAsPNG(){
+        try {
+            OutputStream outs = new FileOutputStream(new File("src/main/resources/200.png"));
+            assert ImgUtil.saveImageAsPNG(bufImage, outs);
+            outs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
