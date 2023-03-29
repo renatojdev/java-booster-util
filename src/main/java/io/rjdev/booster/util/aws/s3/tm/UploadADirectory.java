12617c94ee7518bfc51fee1e5b3b3c98b91efade
Renato P Eduardo Jr <renatojr@outlook.com>
2023-03-29 07:28:09 -0300

    Minor improvements in upload a directory

--- src/main/java/io/rjdev/booster/util/aws/s3/tm/UploadADirectory.java
+++ src/main/java/io/rjdev/booster/util/aws/s3/tm/UploadADirectory.java
@@ -12,18 +12,21 @@ package io.rjdev.booster.util.aws.s3.tm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.rjdev.booster.util.Resource;
 import io.rjdev.booster.util.aws.s3.S3ClientFactory;
+import io.rjdev.booster.util.file.FileUtil;
 import io.rjdev.booster.util.string.StringUtil;
+import software.amazon.awssdk.services.s3.model.DeleteBucketResponse;
 import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
 import software.amazon.awssdk.transfer.s3.model.CompletedDirectoryUpload;
 import software.amazon.awssdk.transfer.s3.model.DirectoryUpload;
 import software.amazon.awssdk.transfer.s3.model.UploadDirectoryRequest;
 
-import java.net.URL;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
-// snippet-end:[s3.tm.java2.uploadadirectory.import]
 
 /**
  * Before running this Java V2 code example, set up your development environment, including your credentials.
@@ -35,8 +38,8 @@ import java.util.UUID;
 
  public class UploadADirectory {
     private static final Logger logger = LoggerFactory.getLogger(UploadADirectory.class);
-    public final String bucketName = "x-" + UUID.randomUUID();
-    public String sourceDirectory;
+    private String bucketName = "x-" + UUID.randomUUID();
+    private String sourceDirectory;
 
     public UploadADirectory() {
         setUp();
@@ -48,15 +51,21 @@ import java.util.UUID;
     }
 
     public Integer upload(String sourceDirectory){
+        return upload(sourceDirectory, "");
+    }
+
+    public Integer upload(String sourceDirectory, String bucketName){
         if(StringUtil.isNotBlank(sourceDirectory))
             this.sourceDirectory = sourceDirectory;
+        if(StringUtil.isNotBlank(bucketName))
+            this.bucketName = bucketName;
         return upload();
     }
 
     public Integer upload() {
         Integer numFailedUploads = uploadDirectory(S3ClientFactory.transferManager, sourceDirectory, bucketName);
         logger.info("Number of failed transfers [{}].", numFailedUploads);
-        cleanUp();
+        // cleanUp();
         return numFailedUploads;
     }
 
@@ -78,19 +87,26 @@ import java.util.UUID;
 
     private void setUp(){
         S3ClientFactory.s3Client.createBucket(b -> b.bucket(bucketName));
-        URL dirResource = UploadADirectory.class.getClassLoader().getResource("data");
-        sourceDirectory = dirResource.getPath();
+        sourceDirectory = Resource.get("default_upload_dir");
     }
 
-    public void cleanUp(){
-        System.out.println("cleanUP: "+ bucketName);
+    public boolean cleanUp(){
+        System.out.println("cleanUP: "+ bucketName + " sourceDir: "+ sourceDirectory);
+        FileUtil fu = new FileUtil(sourceDirectory);
+        Set<String> filesSet = fu.listNameFiles();
+        Set<ObjectIdentifier> objects = new HashSet<>();
+                filesSet.stream().forEach(name -> {
+                    objects.add(ObjectIdentifier.builder().key(name).build());
+                });
         S3ClientFactory.s3Client.deleteObjects(b -> b
             .bucket(bucketName)
             .delete(b1 -> b1
                 .objects(
-                    ObjectIdentifier.builder().key("f1.txt").build(),
-                    ObjectIdentifier.builder().key("file-test-s3.txt").build()
+                    objects
                 )));
-        S3ClientFactory.s3Client.deleteBucket(b -> b.bucket(bucketName));
+        DeleteBucketResponse resp = S3ClientFactory.s3Client
+            .deleteBucket(b -> b.bucket(bucketName));
+        return resp.sdkHttpResponse().isSuccessful();
+
     }
 }
--- src/test/java/io/rjdev/booster/util/aws/s3/UploadADirectoryTest.java
+++ src/test/java/io/rjdev/booster/util/aws/s3/UploadADirectoryTest.java
@@ -1,28 +1,54 @@
 package io.rjdev.booster.util.aws.s3;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.rjdev.booster.util.Resource;
 import io.rjdev.booster.util.aws.s3.tm.UploadADirectory;
 
 public class UploadADirectoryTest {
-
+    static UploadADirectory up;
     static String access_key;
     static String secret_access_key;
+    static String bucket_name;
+    static String sourceDirectory;
 
     @BeforeAll
     public static void start() {
         System.out.println("=======Starting junit 5 tests========");
         access_key = Resource.get("access_key");
         secret_access_key = Resource.get("secret_access_key");
+        bucket_name = Resource.get("default_bucket_name");
+        sourceDirectory = Resource.get("default_upload_dir");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        up = new UploadADirectory(access_key, secret_access_key);
+    }
+
+    @AfterEach
+    public void end(){
+        up=null;
     }
 
     @Test
     public void test_uploadADir(){
-        UploadADirectory up = new UploadADirectory(access_key, secret_access_key);
         assert(0 == up.upload());
+        assert(up.cleanUp());
     }
 
+    @Test
+    public void test_uploadADirSourceDir(){
+        assert(0 == up.upload(sourceDirectory));
+        assert(up.cleanUp());
+    }
+
+    @Test
+    public void test_uploadADirBucketName(){
+        assert(0 == up.upload(null, bucket_name));
+    }
 
 }
